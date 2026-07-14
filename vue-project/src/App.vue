31e37058ba5f5986ad4accfe59cd76a8655d4632
aaron03emnify <aaron.engist@emnify.com>
2026-07-14 13:31:40 +0200

    landing page

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,10 +5,9 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="aaron's morse code converter" />
     </div>
   </header>
 
@@ -19,7 +18,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <style scoped>
 header {
-  line-height: 1.5;
+  line-height: 1.3;
 }
 
 .logo {
